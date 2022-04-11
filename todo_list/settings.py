6ba6b9f19df9aed5abce18cd40f29819350d1cca
ellis3684 <ellis3684@yahoo.com>
2022-04-11 22:17:12 +0800

    Add django-heroku

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -142,3 +143,5 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # AWS_S3_SIGNATURE_VERSION = 's3v4'
 # AWS_S3_FILE_OVERWRITE = False
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
