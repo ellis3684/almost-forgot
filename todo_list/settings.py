b54b35dbc67950ffd5a854d0dbabbf6e5c801523
ellis3684 <ellis3684@yahoo.com>
2022-04-11 22:11:23 +0800

    Add heroku to allowed hosts

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_TODO_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://almostforgot.herokuapp.com/']
+ALLOWED_HOSTS = ['almostforgot.herokuapp.com']
 
 
 # Application definition
