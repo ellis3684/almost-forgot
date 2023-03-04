3dae4f9aaa14fbc0454e40d71c81577991e38528
Corey Ellis <ellis3684@yahoo.com>
2023-03-04 00:27:52 -0500

    Create list view tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -9,6 +9,8 @@ from .models import Task
 class TaskTests(TestCase):
     username = 'fake_user123'
     pw = 'this1is2not3a4real5user@'
+    task_title = 'Clean the house'
+    category = 'General'
     tomorrow = datetime.date.today() + datetime.timedelta(days=1)
 
     @classmethod
@@ -28,18 +30,53 @@ class TaskTests(TestCase):
 
     def test_create_task_valid(self):
         self.client.login(username=self.username, password=self.pw)
-        task_title = 'Clean the house'
         self.client.post(reverse('task-create'), data={
-            'task': task_title,
+            'task': self.task_title,
             'category': 'General',
             'due_date': self.tomorrow
         })
-        self.assertEqual(Task.objects.last().task, task_title)
+        self.assertEqual(Task.objects.last().task, self.task_title)
 
     def test_create_task_invalid_missing_task_title(self):
         self.client.login(username=self.username, password=self.pw)
         self.client.post(reverse('task-create'), data={
-            'category': 'General',
+            'category': self.category,
             'due_date': self.tomorrow
         })
         self.assertEqual(Task.objects.all().count(), 0)
+
+    def test_create_task_invalid_user_not_logged_in(self):
+        self.client.post(reverse('task-create'), data={
+            'task': self.task_title,
+            'category': self.category,
+            'due_date': self.tomorrow
+        })
+        self.assertEqual(Task.objects.all().count(), 0)
+
+    def test_list_all_tasks(self):
+        self.client.login(username=self.username, password=self.pw)
+        task1 = Task.objects.create(
+            task=self.task_title,
+            category=self.category,
+            due_date=self.tomorrow,
+            owner=self.user
+        )
+        task2 = Task.objects.create(
+            task='Submit timesheet',
+            category='Work',
+            due_date=self.tomorrow + datetime.timedelta(days=2),
+            owner=self.user
+        )
+        response = self.client.get(reverse('tasks'))
+        context_tasks = response.context['tasks']
+        self.assertEqual(context_tasks[0].task, task1.task)
+        self.assertEqual(context_tasks[1].task, task2.task)
+
+    def test_list_tasks_empty(self):
+        self.client.login(username=self.username, password=self.pw)
+        response = self.client.get(reverse('tasks'))
+        context_tasks = response.context['tasks']
+        self.assertQuerysetEqual(context_tasks, [])
+        self.assertContains(response, 'You have no tasks yet.')
+
+
