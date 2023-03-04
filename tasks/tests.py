5e3c25dbc127799c499b2f84c4f3fe5aa5204e84
Corey Ellis <ellis3684@yahoo.com>
2023-03-03 23:04:41 -0500

    Create task home page and create task tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,37 @@
+import datetime
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from .forms import TaskForm
+from .models import Task
 
-# Create your tests here.
+
+class TaskTests(TestCase):
+    username = 'fake_user123'
+    pw = 'this1is2not3a4real5user@'
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(cls.username, password=cls.pw)
+
+    def test_home_page_unauthenticated(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tasks/home.html')
+
+    def test_home_page_authenticated(self):
+        self.client.login(username=self.username, password=self.pw)
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('tasks'))
+
+    def test_create_task(self):
+        self.client.login(username=self.username, password=self.pw)
+        tomorrow = datetime.datetime.today() + datetime.timedelta(days=1)
+        task_title = 'Clean the house'
+        self.client.post(reverse('task-create'), data={
+            'task': task_title,
+            'category': 'General',
+            'due_date': tomorrow
+        })
+        self.assertEqual(Task.objects.last().task, task_title)
